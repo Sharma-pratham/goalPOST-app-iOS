cc292cef97c1a32d19964e34cd1c8276811d8b26
Pratham Sharma <44340689+Sharma-pratham@users.noreply.github.com>
2020-05-07 00:02:35 +0530

    Extensions for UIView and UIButton

---
+++ Extensions/UIButtonExt.swift
@@ -0,0 +1,19 @@
+//
+//  UIButtonExt.swift
+//  goalpost-app
+//
+//  Created by Aditya Sharma on 06/05/20.
+//  Copyright © 2020 Aditya Sharma. All rights reserved.
+//
+
+import UIKit
+extension UIButton {
+    func setSelectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.3097702216, green: 0.6003848376, blue: 1, alpha: 1)
+    }
+    
+    func setDeslectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.4572432793, green: 0.5924020195, blue: 0.8705882353, alpha: 1)
+    }
+    
+}
