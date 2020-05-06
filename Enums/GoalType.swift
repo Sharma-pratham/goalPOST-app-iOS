d79acb2b72e467f29a71c4522c2d0f864c4f9611
Pratham Sharma <44340689+Sharma-pratham@users.noreply.github.com>
2020-05-07 00:04:31 +0530

    An Enum for goal type

---
+++ Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType\.swift
+//  goalpost-app
+//
+//  Created by Aditya Sharma on 02/05/20.
+//  Copyright © 2020 Aditya Sharma. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
