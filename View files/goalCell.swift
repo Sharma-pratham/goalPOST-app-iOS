f42dd6953980e038d4242b3b4fffb77f7edfea15
Pratham Sharma <44340689+Sharma-pratham@users.noreply.github.com>
2020-05-14 19:23:46 +0530

    Updated to work with CoreData

--- View files/goalCell.swift
+++ View files/goalCell.swift
@@ -6,6 +6,7 @@ class goalCell: UITableViewCell {
     @IBOutlet weak var goalType: UILabel!
     @IBOutlet weak var goalProgress: UILabel!
     
+    
     func configureCell(description: String, type: GoalType , goalProgressAmount: Int) {
         self.goalDescription.text = description
         self.goalType.text = type.rawValue
