88eb2c688998be827044a03b805cdd629940cad9
Pratham Sharma <44340689+Sharma-pratham@users.noreply.github.com>
2020-05-14 19:22:07 +0530

    Updated variable change

--- Extensions/UIViewControllerExt.swift
+++ Extensions/UIViewControllerExt.swift
@@ -1,3 +1,11 @@
+//
+//  UIViewContollerExt.swift
+//  goalpost-app
+//
+//  Created by Aditya Sharma on 03/05/20.
+//  Copyright © 2020 Aditya Sharma. All rights reserved.
+//
+
 import UIKit
 
 extension UIViewController {
@@ -23,6 +31,7 @@ extension UIViewController {
             self.present(viewControllerToPresent, animated: false, completion: nil)
         }
     }
+    
     func dismissDetail() {
         let transition = CATransition()
         transition.duration = 0.3
